207d484828f43e5fb2c13c7c82e8cf0c8030ae4f
Faheedat Abdulsalam O <fahidah.abdussalam@gmail.com>
2022-08-16 17:38:05 +0100

    little improvement on the spawn transform

--- Assets/Scripts/CoachController.cs
+++ Assets/Scripts/CoachController.cs
@@ -8,13 +8,13 @@ public class CoachController : MonoBehaviour
 
     public GameObject slamBall; //weighted ball to be spawned
     public Transform spawnPoint; //point where the slam balls will spawn
-    public float launchVelocity = 5.0f; // the force at which the slamBall spawns
+    public float launchVelocity = 7000.0f; // the force at which the slamBall spawns
                                     
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("SpawnSlamBalls", 1.0f, 1.0f);
     }
 
     // Update is called once per frame
@@ -26,7 +26,7 @@ public class CoachController : MonoBehaviour
     void SpawnSlamBalls()
     {
         GameObject spawnedSlamBall = Instantiate(slamBall, spawnPoint.position, spawnPoint.rotation);
-        _ = spawnedSlamBall.GetComponent<Rigidbody>().AddRelativeForce(new Vector3(0, launchVelocity, 0));
+        spawnedSlamBall.GetComponent<Rigidbody>().AddRelativeForce(new Vector3(0, launchVelocity, 0));
            
     }
 }
